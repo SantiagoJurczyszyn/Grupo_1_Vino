851f73d52adf4f00948533a412ea7b3547262329
Facunditos <facundolopezcrespo302@gmail.com>
2021-10-06 11:03:09 -0300

    subí formularios

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'views/index.html'
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'views/shop.html'))});
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'views/product.html'))});
 app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'views/cart.html'))});
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'views/register.html'))});
-app.get('/', (req, res) => {res.sendFile(path.join(__dirname, 'views/login.html'))});
+app.get('/register', (req, res) => {res.sendFile(path.join(__dirname, 'views/register.html'))});
+app.get('/login', (req, res) => {res.sendFile(path.join(__dirname, 'views/login.html'))});
 
 
 app.use(express.static('public'));
