e624cb5faba35fc770c63c7e64fcf7a1866d7375
victoriariso <victoriariso@gmail.com>
2021-11-06 02:35:52 -0300

    configuracion app para usar metodos POST, PUT y DELETE

--- src/app.js
+++ src/app.js
@@ -5,6 +5,20 @@ const productRouter = require ("./routes/productRouter");
 const userRouter = require ("./routes/userRouter");
 const path = require('path')
 
+/*Para enviar peticiones por POST es necesario tener un formulario
+desde el que se enviaran los datos. Hay que configurar el entorno de 
+nuestra aplicación para que sea capaz de capturar esa información
+con estas dos lineas*/
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+  
+/*Los métodos PUT y DELETE no son soportados por los formularios de HTML
+Hay que configurar app.js para poder sobreescribir el método original e 
+implementar los métodos PUT o DELETE*/
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+  
+
 app.use(express.static('public'));
 app.set ("view engine", "ejs");
 app.set ("views", "./src/views")
