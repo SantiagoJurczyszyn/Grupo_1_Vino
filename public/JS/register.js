91d58c99fbff9fce914a1eab6f31c687fc3d48d2
Facunditos <facundolopezcrespo302@gmail.com>
2022-01-21 14:50:43 -0300

    optimicé register.js

--- public/JS/register.js
+++ public/JS/register.js
@@ -34,41 +34,64 @@ window.addEventListener("load",function () {
     const imageUser=document.querySelector("#imageUser")
     const campoImageUser=document.querySelector("#campoImageUser")
     
+    //creo los elemento que contiene los respectivos mensajes de error
+    const first_nameError=document.createElement("p")
+    first_nameError.setAttribute("id","first_nameError")
+    first_nameError.textContent="El nombre ingresado debe comenzar con una mayuscula y contener dos caracteres como mínimo"
+    first_nameError.classList="texto-error"
+
+    const last_nameError=document.createElement("p")
+    last_nameError.setAttribute("id","last_nameError")
+    last_nameError.textContent="El apellido ingresado debe comenzar con una mayuscula y contener dos caracteres como mínimo"
+    last_nameError.classList="texto-error"
+    
+    const emailRegistrado=document.createElement("p")
+    emailRegistrado.setAttribute("id","emailRegistrado")
+    emailRegistrado.textContent="Ya existe una cuenta con este correo electrónico, iniciá sesión con la contraseña ingresada oportunamente"
+    emailRegistrado.classList="texto-error"
+
+    const emailError=document.createElement("p")
+    emailError.setAttribute("id","emailError")
+    emailError.textContent="Debe ingresar un email válido"
+    emailError.classList="texto-error"
+
+    const passwordError=document.createElement("p")
+    passwordError.setAttribute("id","passwordError")
+    passwordError.textContent="Debe ingresar una contraseña que contenga al menos ocho caracteres, una letra minúscula, una letra mayúscula, un número y un caracter especial (símbolo)"
+    passwordError.classList="texto-error"
+            
+    const categoryError=document.createElement("p")
+    categoryError.setAttribute("id","categoryError")
+    categoryError.textContent="Debe ingresar una categoría"
+    categoryError.classList="texto-error"
+            
     //según la validación del campo, el borde se va a pintar de un color o de otro
     const bordeRojo="2px solid red";
     const bordeVerde="2px solid limegreen"
+
+    //declaración de función utilizada a ejecutarse ante el evento "blur"
+    
+    function validar (div,input,mensajeError) {
+        const valorIngresado=input.value
+        const extensionCorrecta=validator.isLength(valorIngresado,{ min: 2})
+        const usoMayuscula=validator.isStrongPassword(valorIngresado,{ minLength: 0, minLowercase: 0, minUppercase: 1, minNumbers: 0, minSymbols: 0 })
+        if (!(extensionCorrecta && usoMayuscula)) {
+            div.appendChild(mensajeError)
+            return input.style.border=bordeRojo
+        } else {
+            return input.style.border=bordeVerde
+        }
+    }
     
     
     //para todos los campos aplico la misma lógica, una vez que el mismo es completado, se corroba si el valor ingresado pasa las restricciones establecisas, si no lo hace, se informa las restricciones que deben contemplarse y el borde se pinta de rojo, si lo hace, el borde se pinta de verde 
     first_name.addEventListener("blur",(evento)=>{
-        const valorIngresado=first_name.value
-        const extensionCorrecta=validator.isLength(valorIngresado,{ min: 2})
-        const usoMayuscula=validator.isStrongPassword(valorIngresado,{ minLength: 0, minLowercase: 0, minUppercase: 1, minNumbers: 0, minSymbols: 0 })
-        if (!(extensionCorrecta && usoMayuscula)) {
-            const firt_nameError=document.createElement("p")
-            firt_nameError.textContent="El nombre ingresado debe comenzar con una mayuscula y contener dos caracteres como mínimo"
-            firt_nameError.classList="texto-error"
-            campoFirst_name.appendChild(firt_nameError)
-            first_name.style.border=bordeRojo
-        } else {
-            first_name.style.border=bordeVerde
-        }
+        validar(campoFirst_name,first_name,first_nameError)
     })
 
     
     last_name.addEventListener("blur",(evento)=>{
-        const valorIngresado=last_name.value
-        const extensionCorrecta=validator.isLength(valorIngresado,{ min: 2})
-        const usoMayuscula=validator.isStrongPassword(valorIngresado,{ minLength: 0, minLowercase: 0, minUppercase: 1, minNumbers: 0, minSymbols: 0 })
-        if (!(extensionCorrecta && usoMayuscula)) {
-            const last_nameError=document.createElement("p")
-            last_nameError.textContent="El apellido ingresado debe comenzar con una mayuscula y contener dos caracteres como mínimo"
-            last_nameError.classList="texto-error"
-            campoLast_name.appendChild(last_nameError)
-            last_name.style.border=bordeRojo
-        } else {
-            last_name.style.border=bordeVerde
-        }
+        validar(campoLast_name,last_name,last_nameError)
     })
 
 
@@ -83,15 +106,9 @@ window.addEventListener("load",function () {
             const emailCorrecto=validator.isEmail(valorIngresado)
             const usuarioRegistrado=arrayUusuarios.find(usuario=>usuario.email==valorIngresado)
             if (usuarioRegistrado) {
-                const emailRegistrado=document.createElement("p")
-                emailRegistrado.textContent="Ya existe una cuenta con este correo electrónico, iniciá sesión con la contraseña ingresada oportunamente"
-                emailRegistrado.classList="texto-error"
                 campoEmail.appendChild(emailRegistrado)
                 email.style.border=bordeRojo
             } else if (!emailCorrecto) {
-                const emailError=document.createElement("p")
-                emailError.textContent="Debe ingresar un email válido"
-                emailError.classList="texto-error"
                 campoEmail.appendChild(emailError)
                 email.style.border=bordeRojo
             } else {
@@ -106,9 +123,6 @@ window.addEventListener("load",function () {
         const valorIngresado=password.value
         const passwordCorrecta=validator.isStrongPassword(valorIngresado,{ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })
         if (!passwordCorrecta) {
-            const passwordError=document.createElement("p")
-            passwordError.textContent="Debe ingresar una contraseña que contenga al menos ocho caracteres, una letra minúscula, una letra mayúscula, un número y un caracter especial (símbolo)"
-            passwordError.classList="texto-error"
             campoPassword.appendChild(passwordError)
             password.style.border=bordeRojo
         } else {
@@ -121,9 +135,6 @@ window.addEventListener("load",function () {
         const categoryUser=validator.equals(valorIngresado,"User")
         const categoryAdmin=validator.equals(valorIngresado,"Admin")
         if (!(categoryUser || categoryAdmin)) {
-            const categoryError=document.createElement("p")
-            categoryError.textContent="Debe ingresar una categoría"
-            categoryError.classList="texto-error"
             campoCategory.appendChild(categoryError)
             category.style.border=bordeRojo
         } else {
@@ -151,6 +162,28 @@ window.addEventListener("load",function () {
             listaErrores.style.listStyle="none"
             errores.forEach(error=>listaErrores.innerHTML+="<li class=elementoLista >"+error+"</li>")
             campoErrorres.appendChild(listaErrores)
+            if (first_name.style.border==bordeVerde && document.getElementById("first_nameError")) {
+                first_nameError.innerHTML=""
+            } 
+            if (last_name.style.border==bordeVerde && document.getElementById("last_nameError")) {
+                last_nameError.innerHTML=""
+            } 
+            if (email.style.border==bordeVerde && document.getElementById("emailError")) {
+                emailError.innerHTML=""
+            } 
+            if (email.style.border==bordeVerde && document.getElementById("emailRegistrado")) {
+                emailRegistrado.innerHTML=""
+            } 
+            if (password.style.border==bordeVerde && document.getElementById("passwordError")) {
+                passwordError.innerHTML=""
+            } 
+            if (category.style.border==bordeVerde && document.getElementById("categoryError")) {
+                categoryError.innerHTML=""
+            } 
+            if (imageUser.style.border==bordeVerde && document.getElementById("imageUserError")) {
+                imageUserError.innerHTML=""
+            } 
+            
 
         }
         
