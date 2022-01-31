35bfffada14a3dcbfc558a10f5c775ba59b1baa0
jurcho91 <90111442+jurcho91@users.noreply.github.com>
2022-01-31 06:03:17 -0300

    API detalle de usuario

--- src/controllers/api/usersAPIControllers.js
+++ src/controllers/api/usersAPIControllers.js
@@ -1,5 +1,7 @@
-const db=require("../../database/models/index")
+const db = require("../../database/models/index.js")
+
 const usersAPIControllers={
+
     list:(req,res)=>{
         db.User.findAll()
         .then(users=>{
@@ -16,7 +18,60 @@ const usersAPIControllers={
         .catch(error=>{console.log(error)})
         
         
+    },
+
+    detail: (req,res) => {
+        /*
+        ○ Deberá devolver un objeto literal con la siguiente estructura:
+        ■ Una propiedad por cada campo en base.
+        ■ Una URL para la imagen de perfil (para mostrar la imagen).
+        ■ Sin información sensible (ej: password y categoría).*/
+        
+        let id = Number(req.params.id);
+        
+
+        db.User.findByPk(id, {attributes: ['first_name', 'last_name', 'email', 'image']})
+        .then(user => {
+        
+        let image = user.image;
+
+        if(user){ 
+    
+        let response = {
+            meta: {
+                status: 200
+            },
+            data: {
+                id : user.id,
+                first_Name : user.first_name,
+                last_Name : user.last_name,
+                email : user.email,
+                url_image : `http://localhost:3030/img/users-img/${image}`
+    
+            }
+        }
+        return res.json(response)
+        }    else {
+                    res.status(404).json({
+                        meta : {
+                            status : 404,
+                            statusMsg : "User not found"
+                        },
+                        data : []
+                    });
+                }
+    
+            })
+            .catch(error => {
+                res.status(500).json({
+                    meta : {
+                        status : 500,
+                        statusMsg : "Internal server error"
+                    },
+                    data : []
+                });
+            }); 
     }
 }
 
-module.exports=usersAPIControllers
+module.exports = usersAPIControllers
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,6 +2,8 @@ const express=require("express")
 const router=express.Router()
 const usersAPIControllers=require("../../controllers/api/usersAPIControllers")
 
+
 router.get("/",usersAPIControllers.list)
+router.get("/:id", usersAPIControllers.detail)
 
 module.exports=router
