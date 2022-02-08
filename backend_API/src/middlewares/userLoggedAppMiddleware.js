edbbf5ef5ad63a1560477c87bf5b28b8200c6663
Facundo <facundolopezcrespo@hotmail.com>
2022-02-08 11:51:17 -0300

    modifiqué middleware de aplicación para consultar la DB, y no el viejo JSON

--- backend_API/src/middlewares/userLoggedAppMiddleware.js
+++ backend_API/src/middlewares/userLoggedAppMiddleware.js
@@ -1,23 +1,35 @@
 const User = require('./User.js')
+const db=require("../database/models/index")
 
 function userLoggedAppMiddleware(req, res, next) {
+   
+
    res.locals.userIsLogged = false;
 
    let emailInCookie = req.cookies.userEmail;
-   let userFromCookie = User.findByField('email', emailInCookie);
-
-   if (userFromCookie) {
-      req.session.userLoggedIn = userFromCookie;
+   if (emailInCookie) {
+      db.User.findOne({
+      where:{
+         email:emailInCookie
+      }
+      })
+      .then(user=>{
+         if (user) {
+         req.session.userLoggedIn = user;
+         }
+         if (req.session.userLoggedIn) {
+         res.locals.userIsLogged = true;
+         // lo pasa a locals para poder mostrarlo en el header
+         res.locals.userLoggedIn = req.session.userLoggedIn;
+         }
+         next();
+      })
+      .catch(error=>console.log(error));
+   } else {
+    next();  
    }
-   if (req.session.userLoggedIn) {
-      res.locals.userIsLogged = true;
-      // lo pasa a locals para poder mostrarlo en el header
-      res.locals.userLoggedIn = req.session.userLoggedIn;
-   }
-
-
-
-   next();
+   
+   
 }
 
 module.exports = userLoggedAppMiddleware
